c49b86497a29caf07dc225d6e6d9cb706ab197f3
火水 <66009908+huoshuiyo@users.noreply.github.com>
2022-03-10 08:09:22 +0800

    E ver

---
+++ Assets/Scripts/E/EButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EButton : MonoBehaviour
+{
+    public GameObject EPanel;
+
+    public void OpenEPanel() 
+    {
+        EPanel.SetActive(true);
+    }
+
+    public void CloseEPanel()
+    {
+        EPanel.SetActive(false);
+    }
+}
