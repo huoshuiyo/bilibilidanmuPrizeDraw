72b632327341d95dc24a7e0b3cd543cca7becf1e
火水 <6101612@qq.com>
2022-06-09 21:30:25 +0800

    Add TimeShow

---
+++ Assets/Scripts/timeNow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class timeNow : MonoBehaviour
+{
+    public Text timeText;
+    private void Awake()
+    {
+        timeText = GetComponent<Text>();
+    }
+    void Update()
+    {
+        DateTime dateTime = DateTime.Now;
+        timeText.text = string.Format("{0:D}-{1:D}-{2:D} {3:D}:{4:D}:{5:D}", dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, dateTime.Second);
+    }
+
+}
