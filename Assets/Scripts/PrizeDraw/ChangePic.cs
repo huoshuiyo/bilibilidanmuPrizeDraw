9bdf606d05bbb9d6235fa05fc0552f7cd43c3749
火水 <66009908+huoshuiyo@users.noreply.github.com>
2022-08-11 23:35:06 +0800

    Secret Update

---
+++ Assets/Scripts/PrizeDraw/ChangePic.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+public class ChangePic : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public CanvasGroup canvasGroup;
+    public Image image;
+    int spritesNum = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasGroup= GetComponent<CanvasGroup>();
+        spritesNum = 0;
+        image = transform.Find("ShowImage").GetComponent<Image>();
+        image.sprite = sprites[spritesNum];
+        transform.Find("BackButton").GetComponent<Button>()
+            .onClick.AddListener(() =>
+            {
+                canvasGroup.alpha = 0;
+                canvasGroup.blocksRaycasts = false;
+                canvasGroup.interactable = false;
+            }
+            );
+        transform.Find("NextButton").GetComponent<Button>()
+            .onClick.AddListener(() =>
+            {
+                spritesNum++;
+                if (spritesNum == sprites.Length)
+                {
+                    spritesNum = 0;
+                }
+                image.sprite = sprites[spritesNum];
+            }
+            );
+
+    }
+    public void OpenPicPanel() 
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.interactable = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
